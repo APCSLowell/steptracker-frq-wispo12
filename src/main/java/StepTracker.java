fde156884f5cb3c39ed9c0cf99cd488846b24e9c
wispo12 <143028172+wispo12@users.noreply.github.com>
2024-04-23 22:31:58 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -1,6 +1,38 @@
 import java.util.ArrayList;
-//public class StepTracker
-//{
- /* to be implemented here */
-//} 
-public class StepTracker 1 /* to be implemented here */ private int minSteps, totalSteps; private int totalDays, actDays; public StepTracker(int goal) minsteps = goal; totalSteps = 0; totalDays = 0; actDays = 0; } public void addDailySteps(int steps) totalSteps += steps; totalDays++; if (steps >= minSteps) actdays++; } public int activeDays() return actDays; } public double averageSteps() { if (totalDays == 0) return 0.0; else return (double)totalSteps / totalDays;}}
+public class StepTracker
+{
+    private final int minStepsActive;
+    private int activeDays;
+    private int days;
+    private int totalSteps;
+
+    public StepTracker(int minStepsActive)
+    {
+        this.minStepsActive = minStepsActive;
+        activeDays = 0;
+        days = 0;
+        totalSteps = 0;
+    }
+
+    public void addDailySteps(int steps)
+    {
+        if(steps >= minStepsActive)
+            activeDays++;
+
+        days++;
+        totalSteps += steps;
+    }
+
+    public int activeDays()
+    {
+        return activeDays;
+    }
+
+    public double averageSteps()
+    {
+        if(days == 0)
+            return 0;
+
+        return totalSteps / (double) days;
+    }
+}
